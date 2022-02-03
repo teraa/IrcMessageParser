5ff04804593c6405380f93496c6960c43f1a43cf
Tera <24725862+teraa@users.noreply.github.com>
2022-02-03 21:24:32 +0100

    code cleanup

--- Irc/Tags.cs
+++ Irc/Tags.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Teraa.Irc;
